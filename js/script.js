75c3a26d029c03aea9435d3145c2f50930a9100f
IshimotoTakara <takara120208@gmail.com>
2021-06-06 00:17:36 +0900

    initial commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function () {
+
+  //ページ内スクロール
+  var $nav = $(".header");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //スクロールに応じてヘッダーの背景色が変化
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.header').addClass('active');
+    } else {
+      $('.header').removeClass('active');
+    }
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
